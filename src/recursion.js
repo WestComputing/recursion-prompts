dfe5aeac61d6551a563a60c013e885b0ad16d217
Julian West <west221b@gmail.com>
2020-02-28 17:04:23 -0500

    Add countValuesInObj

--- src/recursion.js
+++ src/recursion.js
@@ -227,7 +227,13 @@ let countKeysInObj = function (obj, key) {
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
-var countValuesInObj = function (obj, value) {
+let countValuesInObj = function (obj, value) {
+    let count = 0;
+    for (const key in obj) {
+        if (obj[key] === value) count++;
+        if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) count += countValuesInObj(obj[key], value);
+    }
+    return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
