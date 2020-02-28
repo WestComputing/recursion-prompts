3925e2179e5d5b09388cc09897169f501d75bf8e
Julian West <west221b@gmail.com>
2020-02-27 23:33:08 -0500

    Add numToText

--- src/recursion.js
+++ src/recursion.js
@@ -323,7 +323,14 @@ var alternateSign = function (array) {
 // 36. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = function (str) {
+let numToText = function (str) {
+    const dict = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const arr = str.split(' ');
+    let word = arr.shift();
+    let num = parseInt(word);
+    let result = isNaN(num) ? word : dict[num];
+    if (!arr.length) return result;
+    return result + ' ' + numToText(arr.join(' '));
 };
 
 
