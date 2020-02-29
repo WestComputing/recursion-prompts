bf127b75c39557ed9c346e9c97d93982678a163c
Julian West <west221b@gmail.com>
2020-02-28 22:30:47 -0500

    Add nthFibo

--- src/recursion.js
+++ src/recursion.js
@@ -266,7 +266,10 @@ let fibonacci = function (n) {
 // nthFibo(5); // 5
 // nthFibo(7); // 13
 // nthFibo(3); // 2
-var nthFibo = function (n) {
+let nthFibo = function (n) {
+    if (n < 0) return null;
+    if (n < 2) return n;
+    return nthFibo(n - 2) + nthFibo(n - 1);
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
